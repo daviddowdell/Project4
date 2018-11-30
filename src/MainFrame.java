f45781064be0408e0746c4cfd3ffbb8e087a0aea
David Dowdell <david.t.dowdell@ou.edu>
2018-11-30 14:59:09 -0600

    Created GUI and have header in place

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,12 @@
+import java.awt.Color;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  * This is the main, entire frame of the gui
@@ -12,11 +20,74 @@ public class MainFrame extends JFrame
      * Default serial ID
      */
     private static final long serialVersionUID = 1L;
+    /**The panel for the menu bar */
+    JPanel topMenu = new JPanel();
+    /**
+     * The panel for the Heading
+     */
+    JPanel topHeading = new JPanel();
+    /**
+     * Panel for the left column of selectable data types
+     */
+    JPanel datatypes = new JPanel();
+    
+    /**
+     * Panel for the second-from left column of statistic types
+     */
+    JPanel statTypes = new JPanel();
+    
+    /**
+     * Panel for the text box
+     */
+    JPanel textBox = new JPanel();
+    /** Group of Buttons for selecting statistic type */
+    ButtonGroup stats = new ButtonGroup();
+    /** The button to run calculations */
+    JButton calc = new JButton();
+    /** The header for the text output */
+    JLabel columns = new JLabel();
+    /** Header for the whole thing */
+    JLabel mainHeader = new JLabel();
+    /** Textbox for output */
+    JTextField output = new JTextField(5);
+    
+    
+    
+    /**
+     * Inner class for the menu bar
+     */
+    protected class FileMenuBar extends JMenuBar
+    {
+
+        /**
+         * Here is the default ID
+         */
+        private static final long serialVersionUID = 1L;
+        
+        
+        protected FileMenuBar()
+        {
+            super();
+        }
+        
+    }
+    
     
     
     public MainFrame()
     {
         super("Mesonet");
+        
+        // The Setup Section
+        mainHeader.setHorizontalAlignment(JTextField.CENTER);
+  
+        topMenu.add(new FileMenuBar());
+        add(topMenu);
+        
+        mainHeader.setText("Mesonet-we don't set records, we record them!");
+        topHeading.add(mainHeader);
+        topHeading.setBackground(Color.GRAY);
+        add(topHeading);
      // Configuring of the frame
         setSize(400, 400);
         setVisible(true);
