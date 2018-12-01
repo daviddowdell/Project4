f222941bed9acbb39f58aff7aefbe966212b78a7
David Dowdell <david.t.dowdell@ou.edu>
2018-11-30 18:33:40 -0600

    Adjusted some sizing

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,5 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 import javax.swing.BoxLayout;
@@ -19,6 +22,7 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableCellRenderer;
 
 /**
  * This is the main, entire frame of the gui
@@ -45,6 +49,9 @@ public class MainFrame extends JFrame
     /**Title for the datatypes border*/
     TitledBorder dtBorder = new TitledBorder("Parameter");
     
+    /**Panel for the lower buttons*/
+    JPanel options = new JPanel();
+    
     /**
      * Panel for the second-from left column of statistic types
      */
@@ -59,7 +66,9 @@ public class MainFrame extends JFrame
     /** Group of Buttons for selecting statistic type */
     ButtonGroup stats = new ButtonGroup();
     /** The button to run calculations */
-    JButton calc = new JButton();
+    JButton calc = new JButton("Calculate");
+    /** The button to exit*/
+    JButton exit = new JButton("Exit");
     /** The header for the text output */
     JLabel columns = new JLabel();
     /** Header for the whole thing */
@@ -68,6 +77,10 @@ public class MainFrame extends JFrame
     JTable table;
     String[] tableHeaders = new String[] {"Station", "Parameter", "Statistics",
             "Value", "Reporting Stations", "Date"};
+    
+    Object[][] outputData = {{"","","","","",""}} ;
+    
+            
     /** Selectable buttons for Parameter */
     JCheckBox wspd = new JCheckBox("WSPD");
     JCheckBox pres = new JCheckBox("PRES");
@@ -138,14 +151,12 @@ public class MainFrame extends JFrame
         add(topHeading, BorderLayout.NORTH);
         
         //Set up output text
-        String[][] outputData = {{"","","","","",""}};
-        table = new JTable(outputData, tableHeaders);
-        JScrollPane sp = new JScrollPane(table);
-        table.setShowGrid(false);
         
+        table = makeTable(outputData, tableHeaders);
+        JScrollPane sp = new JScrollPane(table);
         textBox.add(sp);
-       
         add(textBox, BorderLayout.EAST);
+        textBox.setPreferredSize(new Dimension(600,400));
         
         
         //Set up the Parameter selection options
@@ -157,6 +168,8 @@ public class MainFrame extends JFrame
         datatypes.add(wspd);
         datatypes.add(pres);
         add(datatypes, BorderLayout.WEST);
+        datatypes.setPreferredSize(new Dimension(80,50));
+        datatypes.setBackground(Color.gray);
         
         //Set up the Statistic selection options
         statTypes.setLayout(new BoxLayout(statTypes, BoxLayout.Y_AXIS));
@@ -168,6 +181,13 @@ public class MainFrame extends JFrame
         statTypes.add(avg);
         statTypes.add(max);
         add(statTypes, BorderLayout.CENTER);
+        statTypes.setBackground(Color.GRAY);
+       // statTypes.setPreferredSize(new Dimension(50,50));
+        
+        //Set up the lower buttons: exit and calculate
+        options.add(calc);
+        options.add(exit);
+        add(options, BorderLayout.SOUTH);
         
         
         
@@ -176,6 +196,39 @@ public class MainFrame extends JFrame
         setVisible(true);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //Exit JButton:
+        exit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Exit();
+            } 
+        });
     }
+
+
+    /**This Method closes the window*/
+    protected void Exit()
+    {
+        dispose(); 
+    }
+    
+    /**This method makes a table, sometimes a new one */
+    protected JTable makeTable(Object[][] data, String[] headers)
+    {
+        JTable a = new JTable(data, headers);
+        ((DefaultTableCellRenderer)a.getTableHeader().getDefaultRenderer())
+        .setHorizontalAlignment(JLabel.LEFT);
+        a.setShowGrid(false);
+        a.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        a.getColumnModel().getColumn(0).setPreferredWidth(45);
+        a.getColumnModel().getColumn(1).setPreferredWidth(70);
+        a.getColumnModel().getColumn(2).setPreferredWidth(80);
+        a.getColumnModel().getColumn(3).setPreferredWidth(60);
+        a.getColumnModel().getColumn(4).setPreferredWidth(120);
+        a.getColumnModel().getColumn(5).setPreferredWidth(500);
+        a.setFillsViewportHeight(true);
+        return a;
+    }
+     
     
 }
